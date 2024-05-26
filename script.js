cfbfe8814ba6c6406221528b652f87588c1b0fde
Constantino Pasquali <163750560+cpasquali@users.noreply.github.com>
2024-05-26 20:19:23 -0300

    se aplico una funcion para calcular el costo de envio

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const avisoEl = document.getElementById("aviso");
 const buttonAdult = document.getElementById("button-adult");
 const buttonMinor = document.getElementById("button-minor");
 const mainEl = document.getElementById("main");
+const citysEl = document.getElementById("citys");
+const mensajeEnvio = document.getElementById("res-envio");
+const buttonEnvio = document.getElementById("btn-envio");
 
 const adultVerification = () => {
   avisoEl.className = "d-none";
@@ -22,3 +25,23 @@ window.addEventListener("beforeunload", function (event) {
   event.preventDefault();
   return message;
 });
+
+const calculateShipping = () => {
+  let totalEnvio = 0;
+  let mensaje = "";
+  if (
+    (citysEl.value === "San Nicolas") |
+    (citysEl.value === "Villa Ramallo") |
+    (citysEl.value === "Villa Campi")
+  ) {
+    totalEnvio = 0;
+    mensaje = "Su envio es gratis!!";
+  } else if ((citysEl.value === "Rosario") | (citysEl.value === "Mendoza")) {
+    totalEnvio = 800;
+    mensaje = "El total de su envio es de 800 pesos";
+  }
+
+  mensajeEnvio.innerHTML = `<h2>${mensaje}</h2>`;
+};
+
+buttonEnvio.addEventListener("click", calculateShipping);
